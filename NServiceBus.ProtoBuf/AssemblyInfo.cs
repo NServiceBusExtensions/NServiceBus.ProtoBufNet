2c02cbcf32830d9b89de4f8b016d1cca5ffd3ca5
Simon Cropp <simon.cropp@gmail.com>
2016-10-15 19:50:13 +1100

    fix nuget lib

--- NServiceBus.ProtoBuf/AssemblyInfo.cs
+++ NServiceBus.ProtoBuf/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NServiceBus.ProtoBuf")]
 [assembly: AssemblyProduct("NServiceBus.ProtoBuf")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
